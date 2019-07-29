132e317d49c9d23dd37782ecbc46b5a39a5157a2
Rocky Santiago <raphael.santiago@icloud.com>
2019-07-29 10:44:41 +0800

    update test description

--- test/app.e2e-spec.js
+++ test/app.e2e-spec.js
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '@src/app';
 
 describe('GET /', () => {
-  it('should successfully return "Hello World"', async () => {
+  it('should successfully return "Hello World!"', async () => {
     const response = await request(app).get('/');
 
     expect(response.status).toBe(200);

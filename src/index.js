059ec18a1af5b049afd59cdb22760690c15092a2
Rocky Santiago <raphael.santiago@icloud.com>
2019-07-29 10:35:52 +0800

    add integration test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import 'dotenv/config';
-import express from 'express';
 
-const app = express();
+import app from './app';
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

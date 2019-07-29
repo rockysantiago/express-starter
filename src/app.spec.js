5b556729447714d00ae8c6acf2e437cceba7dd4c
Rocky Santiago <raphael.santiago@icloud.com>
2019-07-29 12:57:06 +0800

    add unit test

---
+++ src/app.spec.js
@@ -0,0 +1,12 @@
+import request from 'supertest';
+
+import app from './app';
+
+describe('GET /', () => {
+  it('should successfully return "Hello World!"', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello World!');
+  });
+});
